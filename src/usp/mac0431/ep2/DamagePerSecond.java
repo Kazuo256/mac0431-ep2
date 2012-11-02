c230dee2b39ff28c9ee6f356334c894465d0a0e7
Thiago de Gouveia Nunes <gorobaum@gmail.com>
2012-11-02 14:56:19 -0200

    Melhorias na estat

--- src/usp/mac0431/ep2/DamagePerSecond.java
+++ src/usp/mac0431/ep2/DamagePerSecond.java
@@ -6,19 +6,52 @@ import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
 
 public class DamagePerSecond {
 	HashMap<String, SummaryStatistics> dps = new HashMap<String, SummaryStatistics>();
-	HashMap<String, Long> tempo = new HashMap<String, Long>();
+	HashMap<String, Long> tempoNoJogo = new HashMap<String, Long>();
+	HashMap<String, Long> tempoReal = new HashMap<String, Long>();
+	Long tempoInicial;
+
+	public DamagePerSecond(Long tempoInicial) {
+		this.tempoInicial = tempoInicial;
+	}
 
 	public void adicionaDano(String personagem, long dano, long tempo) {
-		dps.get(personagem).addValue(dano);
-		this.tempo.put(personagem, this.tempo.get(personagem) + tempo);
+		this.getDps(personagem).addValue(dano);
+		long tempoReal = tempo - this.getTempoNoJogo(personagem);
+		this.tempoNoJogo.put(personagem, tempo);
+		this.tempoReal.put(personagem, tempoReal);
 	}
 
 	public void revivePersonagem(String personagem, long tempo) {
-		this.tempo.put(personagem, this.tempo.get(personagem)
-				- (tempo - this.tempo.get(personagem)));
+		long tempoReal = tempo - this.getTempoNoJogo(personagem);
+		this.tempoReal.put(personagem, this.getTempoReal(personagem)
+				- (tempoReal - this.getTempoReal(personagem)));
 	}
 
 	public double geraEstatistica(String personagem) {
-		return dps.get(personagem).getSum() / tempo.get(personagem);
+		if (tempoReal.containsKey(personagem)) {
+			return dps.get(personagem).getSum() / tempoReal.get(personagem);
+		}
+		return 0.0;
+	}
+
+	private SummaryStatistics getDps(String personagem) {
+		if (!this.dps.containsKey(personagem)) {
+			this.dps.put(personagem, new SummaryStatistics());
+		}
+		return this.dps.get(personagem);
+	}
+
+	private Long getTempoNoJogo(String personagem) {
+		if (!this.tempoNoJogo.containsKey(personagem)) {
+			this.tempoNoJogo.put(personagem, new Long(this.tempoInicial));
+		}
+		return this.tempoNoJogo.get(personagem);
+	}
+
+	private Long getTempoReal(String personagem) {
+		if (!this.tempoReal.containsKey(personagem)) {
+			this.tempoReal.put(personagem, new Long(0L));
+		}
+		return this.tempoReal.get(personagem);
 	}
 }

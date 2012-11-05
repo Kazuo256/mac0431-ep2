0f2f1f312f376b1b786b15426a6953d6fbc0513c
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-11-05 08:21:14 -0200

    Mudei a entrega... de novo.

--- publish.sh
+++ publish.sh
@@ -4,12 +4,10 @@ outdir=ThiagoWilson
 
 if [ ! -d $outdir ]; then
   mkdir $outdir
-  mkdir $outdir/dpsmeter
 fi
 
 contents="relatorio.pdf proposta.pdf wowlog.txt"
 cp -r $contents $outdir
-cp -r src/ classes/ .project .classpath build.sh download_libs.sh $outdir/dpsmeter
 
 tar -caf $outdir.tar.gz $outdir
 
